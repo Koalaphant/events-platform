7c4472876ed06248282e22615ddb7bf70bb2596f
Koalaphant <andrew.wardjones@icloud.com>
2024-09-30 13:17:24 +0100

    Abstracted link styling to an other component and changed colour scheme

--- SE/events-platform/components/nav-bar/NavBar.tsx
+++ SE/events-platform/components/nav-bar/NavBar.tsx
@@ -1,16 +1,23 @@
-import Link from "next/link";
 import React from "react";
+import NavLink from "./NavLinks";
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "Events", href: "/events" },
+];
 
 export default function NavBar() {
   return (
-    <div className="flex justify-between items-center p-4 bg-rose-500 text-white">
-      <div>
+    <div className="bg-slate-800 text-white">
+      <div className="max-w-5xl mx-auto flex justify-between items-center p-4">
         <h1 className="text-4xl font-extralight">
           Splend<span className="font-semibold">Event</span>
         </h1>
-      </div>
-      <div>
-        <Link href="/">Events</Link>
+        <div className="flex gap-2">
+          {navLinks.map((link) => (
+            <NavLink key={link.label} label={link.label} href={link.href} />
+          ))}
+        </div>
       </div>
     </div>
   );
