a6a97afa04be70054a1f88758e54ca19fce05b27
Koalaphant <andrew.wardjones@icloud.com>
2024-10-08 00:01:25 +0100

    allowing 'zero' to be set as a price as there will be free event options

--- SE/events-platform/src/app/admin/_actions/events.ts
+++ SE/events-platform/src/app/admin/_actions/events.ts
@@ -12,7 +12,7 @@ const imageSchema = fileSchema.refine(
 
 const addSchema = z.object({
   name: z.string().min(1),
-  priceInPence: z.coerce.number().int().min(1),
+  priceInPence: z.coerce.number().int().min(0),
   description: z.string().min(1),
   image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
