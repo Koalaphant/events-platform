ca761fea6aa9a290ce30c8e4d6157a11a18edcdd
Koalaphant <andrew.wardjones@icloud.com>
2024-10-11 15:59:27 +0100

    added customers and sales section in the admin panel

---
+++ SE/events-platform/src/app/admin/orders/page.tsx
@@ -0,0 +1,80 @@
+import db from "@/db/db";
+import { PageHeader } from "../_components/PageHeader";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVertical } from "lucide-react";
+import { DeleteDropdownItem } from "./_components/OrderActions";
+
+function getOrders() {
+  return db.order.findMany({
+    select: {
+      id: true,
+      pricePaidInPence: true,
+      event: { select: { name: true } },
+      user: { select: { email: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export default function OrdersPage() {
+  return (
+    <>
+      <PageHeader>Sales</PageHeader>
+      <OrdersTable />
+    </>
+  );
+}
+
+async function OrdersTable() {
+  const orders = await getOrders();
+  if (orders.length === 0) return <p>No sales found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Event</TableHead>
+          <TableHead>Customer</TableHead>
+          <TableHead>Price Paid</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell>{order.event.name}</TableCell>
+            <TableCell>{order.user.email}</TableCell>
+            <TableCell>
+              {formatCurrency(order.pricePaidInPence / 100)}
+            </TableCell>
+            <TableCell className="text-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropdownItem id={order.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
---
+++ SE/events-platform/src/app/admin/users/page.tsx
@@ -0,0 +1,82 @@
+import db from "@/db/db";
+import { PageHeader } from "../_components/PageHeader";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVertical } from "lucide-react";
+import { DeleteDropdownItem } from "./_components/UserActions";
+
+function getUsers() {
+  return db.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      orders: { select: { pricePaidInPence: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export default function UsersPage() {
+  return (
+    <>
+      <PageHeader>Customers</PageHeader>
+      <UsersTable />
+    </>
+  );
+}
+
+async function UsersTable() {
+  const users = await getUsers();
+  if (users.length === 0) return <p>No customers found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead>Total Spent</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.orders.length}</TableCell>
+            <TableCell>
+              {formatCurrency(
+                user.orders.reduce((sum, o) => o.pricePaidInPence + sum, 0) /
+                  100
+              )}
+            </TableCell>
+            <TableCell className="text-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DeleteDropdownItem id ={user.id}/>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
