cdbf6fd5c99c13dd620077b50fb56eff42cb99d5
Koalaphant <andrew.wardjones@icloud.com>
2024-10-21 15:39:18 +0100

    added a hero section image, title and links

--- SE/events-platform/src/app/(customer)/events/_components/HeroSection.tsx
+++ SE/events-platform/src/app/(customer)/events/_components/HeroSection.tsx
@@ -1,7 +1,29 @@
+import { FaArrowRight } from "react-icons/fa"; // Import the arrow icon
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export function Hero() {
   return (
-    <div className="h-96 mb-12 bg-primary flex flex-col justify-center items-center">
-      <div className="text-white text-4xl font-bold">SplendEvent</div>
+    <div
+      className="h-[250px] md:h-[450px] mb-12 bg-cover bg-center relative flex flex-col justify-center items-center"
+      style={{ backgroundImage: "url('/events-hero-image.jpg')" }}
+    >
+      <div className="absolute inset-0 bg-orange-700 opacity-60"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-orange-700 to-transparent"></div>
+      <div className="relative flex flex-col items-center justify-center h-full mx-8 lg:mx-0">
+        <p className="text-white text-3xl md:text-5xl mb-4 text-center">
+          Unforgettable experiences await
+        </p>
+        <Button
+          asChild
+          className="flex items-center text-xl px-5 py-6 md:text-2xl md:px-9 md:py-7"
+        >
+          <Link href="/events" className="flex items-center">
+            Explore Events
+            <FaArrowRight className="ml-2" />
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
