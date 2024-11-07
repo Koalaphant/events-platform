74aea20d89fc2a63b55f60002942106eb8866f1f
Koalaphant <andrew.wardjones@icloud.com>
2024-11-07 00:41:46 +0000

    added sign in/sign out buttons to nav bar and hidden orders from signed out users

--- SE/events-platform/src/app/(auth)/layout.tsx
+++ SE/events-platform/src/app/(auth)/layout.tsx
@@ -1,14 +1,27 @@
+import { Nav, NavLink } from "@/components/Nav";
 import { auth } from "@clerk/nextjs/server";
+import Footer from "../(customer)/_components/footer";
 import { redirect } from "next/navigation";
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const { userId } = await auth();
 
   if (userId !== null) redirect("/");
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      {children}
+    <div className="flex flex-col min-h-screen">
+      <Nav>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/events">Events</NavLink>
+      </Nav>
+      <main className="flex flex-grow items-center justify-center">
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
--- SE/events-platform/src/app/(customer)/layout.tsx
+++ SE/events-platform/src/app/(customer)/layout.tsx
@@ -1,5 +1,6 @@
 import { Nav, NavLink } from "@/components/Nav";
 import Footer from "./_components/footer";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
@@ -13,7 +14,19 @@ export default function Layout({
       <Nav>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/events">Events</NavLink>
-        <NavLink href="/orders">Orders</NavLink>
+        <SignedIn>
+          <NavLink href="/orders">Orders</NavLink>
+          <UserButton
+            appearance={{
+              elements: {
+                rootBox: "my-3 rounded-full ml-3",
+              },
+            }}
+          />
+        </SignedIn>
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </Nav>
       <main className="flex-grow">{children}</main>
       <Footer />
